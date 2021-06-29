2affffd63c9f8c0dd1568e32cfaeedac187a95e8
Scott Gruby <scott@gruby.com>
2021-06-28 19:52:50 -0700

    % Fixed potential threading issue for authentication

--- SGNetworkClient/Source/NetworkClient+Authentication.swift
+++ SGNetworkClient/Source/NetworkClient+Authentication.swift
@@ -10,7 +10,7 @@ import Foundation
 extension NetworkClient {
     public func urlSession(_ session: URLSession, task: URLSessionTask, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
 
-        if let networkTask = (networkTasks.first {$0.dataTask == task}), let credentials = networkTask.networkRequest.credentials, challenge.protectionSpace.authenticationMethod  == NSURLAuthenticationMethodHTTPBasic || challenge.protectionSpace.authenticationMethod  == NSURLAuthenticationMethodHTTPDigest {
+        if let networkTask = networkTask(for: task), let credentials = networkTask.networkRequest.credentials, challenge.protectionSpace.authenticationMethod  == NSURLAuthenticationMethodHTTPBasic || challenge.protectionSpace.authenticationMethod  == NSURLAuthenticationMethodHTTPDigest {
             completionHandler(.useCredential, credentials)
         } else {
             completionHandler(.performDefaultHandling, nil)
